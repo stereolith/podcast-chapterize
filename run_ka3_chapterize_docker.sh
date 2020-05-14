574838d006c10a5bee3e7334e57d97a05a4b897a
stereolith <mail@lukasmoench.de>
2020-05-14 13:27:16 +0200

    make absPath function solve for ".." shorthand

--- run_ka3_chapterize_docker.sh
+++ run_ka3_chapterize_docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 absPath() {
-    case $1 in
-        /*) echo $1;;
-        *) echo $PWD/$1;;
-    esac
+  # $1 : relative filename
+  if [ -d "${1%/*}" ]; then
+    echo "$(cd ${1%/*}; pwd)/${1##*/}"
+  fi
 }
 
 output="${@: -1}"
+
 
 if [[ $# -eq 0 ]]; then
     echo not enough arguments
@@ -16,14 +17,14 @@ fi
 
 if [[ $1 == "-a" ]]; then
     docker run -it \
-        --mount type=bind,source="$(absPath $2)",target=/app/input/audio \
-        --mount type=bind,source="$(absPath $3)",target=/app/input/transcript \
-        --mount type=bind,source="$(absPath $output)"=/app/output \
+        --mount type=bind,source="$(absPath "$2")",target=/app/input/audio \
+        --mount type=bind,source="$(absPath "$3")",target=/app/input/transcript \
+        --mount type=bind,source="$(absPath "$output")"=/app/output \
         lmnch1113/ka3-chapterize-cli -a /app/input/audio /app/input/transcript /app/output
 else
     docker run -it \
-        --mount type=bind,source="$(absPath $1)",target=/app/input/transcript \
-        --mount type=bind,source="$(absPath $output)",target=/app/output \
+        --mount type=bind,source="$(absPath "$1")",target=/app/input/transcript \
+        --mount type=bind,source="$(absPath "$output")",target=/app/output \
         lmnch1113/ka3-chapterize-cli /app/input/transcript /app/output
 fi
 
